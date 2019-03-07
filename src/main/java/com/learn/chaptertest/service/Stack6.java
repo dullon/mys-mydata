5f21f6eb68ac5e1767ac7f4b1fece56a065df091
dulang5004@sina.com <dulang5004@sina.com>
2019-03-07 17:21:56 +0800

    重构 消除过期的对象引用

---
+++ src/main/java/com/learn/chaptertest/service/Stack6.java
@@ -0,0 +1,41 @@
+package com.learn.chaptertest.service;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * 消除过期的对象引用
+ */
+public class Stack6 {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL = 16;
+
+    public Stack6(){
+        elements = new Object[DEFAULT_INITIAL];
+    }
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+    //弹栈的时候 出来的对象 及时没有被引用 也不会被垃圾回收 ，因为 栈内部会对这些对象维护的过期引用存在内存泄露问题。
+    //2 处理方法 清空被弹出的对象
+    // 但是 方法依然不是最好的处理方法，最好的方法是调用该引用的对象或变量 结束生命周期，本例就是Stack6这个类结束生命周期时 自然会清空所有栈内对象。
+    public Object pop(){
+        if (size == 0){
+            throw new EmptyStackException();
+        }
+        //return  elements[--size];
+        //2
+        Object result = elements[--size];
+        elements[size] = null;//**看似简单的方式 却是解决内存泄露的重点。
+        return result;
+    }
+
+    //扩容时 产生新的数组，引用指向新的 数组，但是 旧的对象依然存在没有只是没有被引用
+    private void ensureCapacity(){
+        if (elements.length == size){
+            elements = Arrays.copyOf(elements,2*size + 1);//扩容把旧的数组迁移到新数组内
+        }
+    }
+}
